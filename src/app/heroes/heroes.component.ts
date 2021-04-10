7df859858844d97f8a086fde5d9d2c61a148d674
Salma73480 <201821820@tese.edu.mx>
2021-04-10 18:15:44 -0500

    Creacion del componente hero-detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES}from '../mock-heroes'
 
-import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +9,11 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
 
   heroes = HEROES;
   selectedHero?: Hero;
+ 
   
   constructor() { }
 
